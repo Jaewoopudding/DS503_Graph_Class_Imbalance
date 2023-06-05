2397eee9f166eadb990f876e8dc2f0a31e74b78a
Jaewoopudding <ijnn258@gmail.com>
2023-06-05 21:05:55 +0900

    [bug fix] trainer function fix

--- utils.py
+++ utils.py
@@ -16,19 +16,19 @@ def train_model(model, data:torch_geometric.data.data.Data, optimizer, criterion
     acc = int(correct.sum()) / int(data.train_mask.sum())
     return loss.detach().cpu().numpy(), acc
 
-def train_constrative_model(model, data:torch_geometric.data.data.Data, optimizer, criterion):
-    CL = ConstrativeLoss(temperature=0.2)
+def train_constrative_model(model, data:torch_geometric.data.data.Data, optimizer, criterion, constrative_coef=2e-3, temperature=0.07):
+    CL = ConstrativeLoss(temperature=temperature)
     model.train()
     optimizer.zero_grad()
     out, embedding = model(data.x, data.edge_index)
-    constrative_loss = CL(embedding[:,:,:70])
+    constrative_loss = constrative_coef * CL(embedding[-70:])
     loss = criterion(out[data.train_mask], data.y[data.train_mask]) + constrative_loss
     loss.backward()
     optimizer.step()
     pred = out.argmax(dim=-1)
     correct = pred[data.train_mask] == data.y[data.train_mask]
     acc = int(correct.sum()) / int(data.train_mask.sum())
-    return loss.detach().cpu().numpy(), constrative_loss.detach().cpu().numpy(), acc
+    return loss.detach().cpu().numpy(), acc
 
 def test_model(model, data:torch_geometric.data.data.Data):
     model.eval()
