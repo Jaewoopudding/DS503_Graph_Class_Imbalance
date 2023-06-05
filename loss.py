14f10e41ebc422e6efc8481ff53c2d5dcd94e3cc
Jaewoopudding <ijnn258@gmail.com>
2023-06-05 22:00:13 +0900

    [New Feature] Triplet loss implemented

--- loss.py
+++ loss.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
 import torch
 from torchmetrics.functional import pairwise_cosine_similarity
+from torch.nn.functional import triplet_margin_loss
+
 
 class ConstrativeLoss(nn.Module):
     def __init__(self, temperature):
@@ -11,3 +13,15 @@ class ConstrativeLoss(nn.Module):
         sim = pairwise_cosine_similarity(x)
         sim = (sim.sum(-1) / self.temperature).mean()
         return sim
+    
+
+class Triplet_on_closest_emb(nn.Module):
+    def __init__(self, ):
+        super().__init__()
+    def forward(self, x):
+        sim = pairwise_cosine_similarity(x)
+        ind  = torch.argmax(sim.flatten()).item()
+        row = ind // sim.shape[0]
+        col = ind % sim.shape[0]
+        loss = triplet_margin_loss(x[row],x[row], x[col])
+        return loss
