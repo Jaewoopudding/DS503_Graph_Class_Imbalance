3598a3122ad6d395a517d117a1bc0e7475dbb1f5
Jaewoopudding <ijnn258@gmail.com>
2023-06-05 22:00:53 +0900

    [Minor Fix] train code bug fix

--- utils.py
+++ utils.py
@@ -37,5 +37,19 @@ def test_model(model, data:torch_geometric.data.data.Data):
     report = classification_report(data.y[~data.train_mask.cpu()].cpu(), pred[~data.train_mask.cpu()].cpu(), output_dict=True)
     return report
 
+def valid_model(model, data:torch_geometric.data.data.Data, criterion, constrative_flag=False ,constrative_coef=2e-3, temperature=0.07):
+    CL = ConstrativeLoss(temperature=temperature)
+    model.eval()
+    out, embedding = model(data.x, data.edge_index)
+
+    loss = criterion(out[data.valid_mask], data.y[data.valid_mask])
+    if constrative_flag:
+        constrative_loss = constrative_coef * CL(embedding[-70:])
+        loss = loss + constrative_loss
+    pred = out.argmax(dim=-1)
+    correct = pred[data.valid_mask] == data.y[data.valid_mask]
+    acc = int(correct.sum()) / int(data.train_mask.sum())
+    return loss.detach().cpu().numpy(), acc
+
 def save_result(report):
     pass
