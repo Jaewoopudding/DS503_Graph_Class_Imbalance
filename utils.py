64f40efc689957dadba146e2a901291b38808e99
Jaewoopudding <ijnn258@gmail.com>
2023-06-06 15:31:50 +0900

    [New Feature] Constrative loss with postive sample training code implemented

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ from sklearn.metrics import f1_score
 from sklearn.metrics import classification_report
 import torch_geometric
 
-from loss import ConstrativeLoss
+from loss import ConstrativeLoss, ConstrativeLosswithPositiveSample
 
 def train_model(model, data:torch_geometric.data.data.Data, optimizer, criterion):
     model.train()
@@ -22,6 +22,20 @@ def train_constrative_model(model, data:torch_geometric.data.data.Data, optimize
     optimizer.zero_grad()
     out, embedding = model(data.x, data.edge_index)
     constrative_loss = constrative_coef * CL(embedding[-70:])
+    loss = criterion(out[data.train_mask], data.y[data.train_mask]) + constrative_loss
+    loss.backward()
+    optimizer.step()
+    pred = out.argmax(dim=-1)
+    correct = pred[data.train_mask] == data.y[data.train_mask]
+    acc = int(correct.sum()) / int(data.train_mask.sum())
+    return loss.detach().cpu().numpy(), acc
+
+def train_constrative_model_with_positive_sample(model, data:torch_geometric.data.data.Data, optimizer, criterion, constrative_coef=2e-3, temperature=0.07):
+    CL = ConstrativeLosswithPositiveSample(temperature=temperature)
+    model.train()
+    optimizer.zero_grad()
+    out, embedding = model(data.x, data.edge_index)
+    constrative_loss = constrative_coef * CL(embedding[-70:], out[CL.sample_class_node(70, data)])
     loss = criterion(out[data.train_mask], data.y[data.train_mask]) + constrative_loss
     loss.backward()
     optimizer.step()
